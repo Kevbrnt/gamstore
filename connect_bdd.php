6dabee15dc5ebcf646f6dc76cc781f4c7f9a1e97
Kevbrnt <157802084+Kevbrnt@users.noreply.github.com>
2024-07-21 02:29:57 +0200

    Update connect_bdd.php

--- connect_bdd.php
+++ connect_bdd.php
@@ -2,7 +2,7 @@
 
 try {
     // Connexion à la base de données
-    $pdo = new PDO('mysql:host=192.168.1.176;dbname=gamestoretp;charset=utf8', 'KBStudio', '2309');
+    $pdo = new PDO('mysql:host=192.168.1.176:8080;dbname=gamestoretp;charset=utf8', 'KBStudio', '2309');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // S'assurer que les caractères sont encodés en UTF-8
     $pdo->exec('SET NAMES utf8');
