bba810efd9c22d1d9ae0ab2806de9485479f3104
Kevbrnt <157802084+Kevbrnt@users.noreply.github.com>
2024-07-21 15:11:32 +0200

    Update retrait.sql

--- MySql/gamestoretp/retrait.sql
+++ MySql/gamestoretp/retrait.sql
@@ -7,3 +7,9 @@ create table gamestoretp.retrait
     code_postal varchar(100) not null
 );
 
+INSERT INTO retrait (adresse, ville, code_postal) VALUES
+                                                       ('Nantes', '5 rue de l\'Arche Sèche', '44000'),
+                                                       ('Lille', '271 rue Léon Gambetta', '59000'),
+                                                       ('Bordeaux', '12 rue des trois-conils', '33000'),
+                                                       ('Paris', '4 boulevard Voltaire', '75011'),
+                                                       ('Toulouse', '11 avenue de l\'URSS', '31400');
