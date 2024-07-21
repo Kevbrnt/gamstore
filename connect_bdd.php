0cd86718f2654b08dbaf5c43fdfc954c4d74f3dd
Kevbrnt <157802084+Kevbrnt@users.noreply.github.com>
2024-07-21 17:08:26 +0200

    Update connect_bdd.php

--- connect_bdd.php
+++ connect_bdd.php
@@ -1,22 +1,9 @@
-<?php /*
-$host = getenv('192.168.1.176') ?: '192.168.1.176';
-$dbname = getenv('gamestoretp') ?: 'gamestoretp';
-$username = getenv('KBStudio') ?: 'KBStudio';
-$password = getenv('2309') ?: '2309';*/
+<?php
 $host = getenv('DATABASE_HOST');
 $dbname = getenv('DATABASE_NAME');
-$username = getenv('DATABASE_USER');
+$user = getenv('DATABASE_USER');
 $password = getenv('DATABASE_PASSWORD');
 
-echo "Tentative de connexion à la base de données...\n";
-echo "Host: $host\n";
-echo "Database: $dbname\n";
-echo "Username: $username\n";
-
-try {
-    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    echo "Connexion réussie!";
-} catch(PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-}
+$dsn = "pgsql:host=$host;dbname=$dbname;";
+$pdo = new PDO($dsn, $user, $password);
 ?>
