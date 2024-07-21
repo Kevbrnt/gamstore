c2009fe15a82dfc51cb42e49cb88d3d1db6b30da
Kevbrnt <157802084+Kevbrnt@users.noreply.github.com>
2024-07-21 15:07:11 +0200

    Update users.sql

--- MySql/gamestoretp/users.sql
+++ MySql/gamestoretp/users.sql
@@ -1,19 +1,25 @@
 create table gamestoretp.users
 (
-    id         int auto_increment
-        primary key,
+    id int auto_increment primary key,
     username   varchar(50)                                                                         not null,
     first_name varchar(50)                                                                         not null,
     last_name  varchar(50)                                                                         not null,
     email      varchar(100)                                                                        not null,
     address    text                                                                                not null,
     password   varchar(255)                                                                        not null,
-    created_at timestamp                                       default current_timestamp()         not null,
+    add_at timestamp                                       default current_timestamp()         not null,
     image_url  varchar(100)                                    default 'asset/profils/default.png' not null,
-    role       enum ('Visiteur', 'Employés', 'Administrateur') default 'Visiteur'                  not null,
+    role enum ('Visiteur', 'Employés', 'Administrateur') default 'Visiteur'                  not null,
     constraint email
         unique (email),
     constraint username
         unique (username)
 );
+
+Insert into gamestoretp.users (username, first_name, last_name, email, address, password, add_at, image_url, role)
+Value ('Gamestore@Visiteur', 'John', 'Doe', 'email@email.com', 'Adresse', 'Gamestore', 'NOW()', 'asset/profils/default.png', 'Visiteur');
+Insert into gamestoretp.users (username, first_name, last_name, email, address, password, add_at, image_url, role)
+Value ('Gamestore@Employés', 'Louis', 'Doe', 'email@email.com', 'Adresse', 'Gamestore', 'NOW()', 'asset/profils/default.png', 'Visiteur');
+Insert into gamestoretp.users (username, first_name, last_name, email, address, password, add_at, image_url, role)
+Value ('Gamestore@Admin', 'Marc', 'Doe', 'email@email.com', 'Adresse', 'Gamstore', 'NOW()', 'asset/profils/default.png', 'Visiteur');
 
