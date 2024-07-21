307a4ee42c49484b5b1a3572da061935ae5f1131
Kevbrnt <157802084+Kevbrnt@users.noreply.github.com>
2024-07-21 14:16:12 +0200

    Update connect_bdd.php

--- connect_bdd.php
+++ connect_bdd.php
@@ -1,14 +1,13 @@
 <?php
+$host = getenv('DATABASE_HOST') ?: '192.168.1.176';
+$dbname = getenv('DATABASE_NAME') ?: 'gamestoretp';
+$username = getenv('DATABASE_USER') ?: 'KBStudio';
+$password = getenv('DATABASE_PASSWORD') ?: '2309';
 
 try {
-    // Connexion à la base de données
-    $pdo = new PDO('mysql:host=192.168.1.176:8080;dbname=gamestoretp;charset=utf8', 'KBStudio', '2309');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // S'assurer que les caractères sont encodés en UTF-8
-    $pdo->exec('SET NAMES utf8');
-} catch (PDOException $e) {
-    // Message d'erreur en cas d'échec de la connexion
-    echo 'Nous avons pas pu vous connecter : ' . $e->getMessage();
-    exit();  // Terminer l'exécution du script si la connexion échoue
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Erreur : " . $e->getMessage());
 }
 ?>
