29cc5d6d8ccf4887ae63e5c0d07ade6ea04ea8d7
Kevbrnt <157802084+Kevbrnt@users.noreply.github.com>
2024-07-23 13:07:29 +0200

    Delete get_games.php

--- get_games.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'connect_bdd.php';
-
-$stmt = $pdo->prepare("SELECT id, name FROM games");
-$stmt->execute();
-$games = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($games);
-?>
